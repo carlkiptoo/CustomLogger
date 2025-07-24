de522527f6ee7bfb7870a480b515537f291779d1
carlkiptoo <carlosiamcarl@gmail.com>
2025-07-24 16:07:00 +0300

    default logger creation

--- src/index.js
+++ src/index.js
@@ -18,5 +18,33 @@ const loggerPresets = {
         theme: 'default',
         colorEnabled: false,
         ...options
+    }),
+
+    silent: (options = {}) => createLogger({
+        outputStream: {
+            log: () => {},
+            error: () => {}
+        },
+        ...options
     })
+};
+
+const defaultLogger = createLogger();
+
+module.exports = {
+    ColoredLogger,
+    LogBuilder,
+    createLogger,
+    loggerPresets,
+    defaultLogger,
+
+    debug: defaultLogger.debug.bind(defaultLogger),
+    info: defaultLogger.info.bind(defaultLogger),
+    warn: defaultLogger.warn.bind(defaultLogger),
+    error: defaultLogger.error.bind(defaultLogger),
+    success: defaultLogger.success.bind(defaultLogger),
+};
+
+if (typeof window !== 'undefined') {
+    window.ColoredLogger = module.exports;
 }
